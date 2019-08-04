955b1e3bf639dc40c0e4a98b75978a830905be60
mlipski00 <michal.lipski2@gmail.com>
2019-08-04 15:52:05 +0200

    Accessing User information

--- oauth2-login/src/main/java/pl/security/project/oauth/LoginController.java
+++ oauth2-login/src/main/java/pl/security/project/oauth/LoginController.java
@@ -2,11 +2,20 @@ package pl.security.project.oauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +27,9 @@ public class LoginController {
       = "oauth2/authorization";
   Map<String, String> oauth2AuthenticationUrls
       = new HashMap<>();
+
+  @Autowired
+  private OAuth2AuthorizedClientService authorizedClientService;
 
   @Autowired
   private ClientRegistrationRepository clientRegistrationRepository;
@@ -38,4 +50,33 @@ public class LoginController {
     model.addAttribute("urls", oauth2AuthenticationUrls);
     return "oauth_login";
   }
+
+  @GetMapping("/loginSuccess")
+  public String getSuccessPage(Model model, OAuth2AuthenticationToken authentication) {
+    OAuth2AuthorizedClient client = authorizedClientService
+        .loadAuthorizedClient(
+            authentication.getAuthorizedClientRegistrationId(),
+            authentication.getName());
+    String userInfoEndpointUri = client.getClientRegistration()
+        .getProviderDetails().getUserInfoEndpoint().getUri();
+
+    if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+      RestTemplate restTemplate = new RestTemplate();
+      HttpHeaders headers = new HttpHeaders();
+      headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+          .getTokenValue());
+      HttpEntity entity = new HttpEntity("", headers);
+      ResponseEntity<Map> response = restTemplate
+          .exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+      Map userAttributes = response.getBody();
+      model.addAttribute("name", userAttributes.get("name"));
+    }
+    return "loginSuccess";
+  }
+
+  @GetMapping("/loginFailure")
+  public String getFailurePage() {
+    return "loginFailure";
+  }
+
 }
