93ea3c0ee5743fdd82cc12ee7e605f786df8843e
mlipski00 <michal.lipski2@gmail.com>
2019-08-05 19:49:48 +0200

    Added zuul proxy

--- oauth-zuul-gateway/src/main/java/com/security/project/zuul/oauthzuulgateway/OauthZuulGatewayApplication.java
+++ oauth-zuul-gateway/src/main/java/com/security/project/zuul/oauthzuulgateway/OauthZuulGatewayApplication.java
@@ -2,7 +2,9 @@ package com.security.project.zuul.oauthzuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class OauthZuulGatewayApplication {
 
