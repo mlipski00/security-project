d6e08ffa369cc53b7ce3ce25934c106b84090620
mlipski00 <michal.lipski2@gmail.com>
2019-08-07 23:19:05 +0200

    redundant adnotation removal

--- oauth-social-login/src/main/java/pl/security/project/oauth/OAuthApplication.java
+++ oauth-social-login/src/main/java/pl/security/project/oauth/OAuthApplication.java
@@ -2,12 +2,10 @@ package pl.security.project.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrix
-@EnableCircuitBreaker
 public class OAuthApplication {
 
 	public static void main(String[] args) {
