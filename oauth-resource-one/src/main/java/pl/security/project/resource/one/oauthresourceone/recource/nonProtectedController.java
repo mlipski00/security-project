e51f8e251198b21392713301c87531d62be21fb3
mlipski00 <michal.lipski2@gmail.com>
2019-08-07 22:02:47 +0200

    simple endpoint

---
+++ oauth-resource-one/src/main/java/pl/security/project/resource/one/oauthresourceone/recource/nonProtectedController.java
@@ -0,0 +1,13 @@
+package pl.security.project.resource.one.oauthresourceone.recource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class nonProtectedController {
+
+  @GetMapping(name = "$non.protected.endpoint.one")
+  public String getExampleNonProtectedResource() {
+    return "Response from non protected endpoint one";
+  }
+}
