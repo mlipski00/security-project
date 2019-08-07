6616482a335b0c0a211dde2a99a6f2766a9eaee4
mlipski00 <michal.lipski2@gmail.com>
2019-08-07 22:55:57 +0200

    Added histrix

--- oauth-social-login/src/main/java/pl/security/project/oauth/OAuthApplication.java
+++ oauth-social-login/src/main/java/pl/security/project/oauth/OAuthApplication.java
@@ -2,8 +2,12 @@ package pl.security.project.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableCircuitBreaker
 public class OAuthApplication {
 
 	public static void main(String[] args) {
--- oauth-social-login/src/main/java/pl/security/project/oauth/protectedresource/ResourceController.java
+++ oauth-social-login/src/main/java/pl/security/project/oauth/protectedresource/ResourceController.java
@@ -1,13 +1,40 @@
 package pl.security.project.oauth.protectedresource;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ResourceController {
 
+  private static final String RESOURCE_FALLBACK_MESSAGE = "Something failed. Resource fallback message.";
+  private static final String MESSAGE = "message";
+  private static final String RESOURCE_VIEW_NAME = "resource";
+
+
+  @Value("${non.protected.endpoint.one}")
+  private String nonProtectedResourceOneUrl;
+
+  @HystrixCommand(fallbackMethod = "resourceFallback", commandKey = "resource", groupKey = "resource")
   @GetMapping("/resource")
-  public String getResourcePage() {
-    return "resource";
+  public ModelAndView getResourcePage() {
+    RestTemplate restTemplate = new RestTemplate();
+    ModelAndView modelAndView = new ModelAndView(RESOURCE_VIEW_NAME);
+
+    ResponseEntity<String> response
+        = restTemplate.getForEntity(nonProtectedResourceOneUrl, String.class);
+
+    modelAndView.addObject(MESSAGE, response);
+    return modelAndView;
+  }
+
+  public ModelAndView resourceFallback() {
+    ModelAndView modelAndView = new ModelAndView(RESOURCE_VIEW_NAME);
+    modelAndView.addObject(MESSAGE, RESOURCE_FALLBACK_MESSAGE);
+    return modelAndView;
   }
 }
