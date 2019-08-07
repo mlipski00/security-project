975b44903eabd90df3ffc725bb74e8f341745a86
mlipski00 <michal.lipski2@gmail.com>
2019-08-07 21:49:47 +0200

    added resource microservice

---
+++ oauth-resource-one/src/main/java/pl/security/project/resource/one/oauthresourceone/OauthResourceOneApplication.java
@@ -0,0 +1,13 @@
+package pl.security.project.resource.one.oauthresourceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthResourceOneApplication.class, args);
+	}
+
+}
---
+++ oauth-resource-one/src/test/java/pl/security/project/resource/one/oauthresourceone/OauthResourceOneApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.security.project.resource.one.oauthresourceone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthResourceOneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
