7e5864a27d9b6e8db11fced056d080b92cb04e33
mlipski00 <michal.lipski2@gmail.com>
2019-08-11 22:39:08 +0200

    added oauth-authorisation-jwt

---
+++ oauth-authorisation-jwt/src/main/java/pl/security/project/authorisation/OauthAuthorisationJwtApplication.java
@@ -0,0 +1,13 @@
+package pl.security.project.authorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorisationJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorisationJwtApplication.class, args);
+	}
+
+}
