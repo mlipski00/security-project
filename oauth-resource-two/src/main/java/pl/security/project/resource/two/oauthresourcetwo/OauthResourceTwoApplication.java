16b9fbf85d53b9b803221905f8b4e5d623f3452f
mlipski00 <michal.lipski2@gmail.com>
2019-08-11 19:52:47 +0200

    added second resource server

---
+++ oauth-resource-two/src/main/java/pl/security/project/resource/two/oauthresourcetwo/OauthResourceTwoApplication.java
@@ -0,0 +1,13 @@
+package pl.security.project.resource.two.oauthresourcetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthResourceTwoApplication.class, args);
+	}
+
+}
